085f51f9d09ce4d36b48288bf8e12724da4d1cae
yashthenua <yashthenua63@gmail.com>
2026-08-14 01:03:46 +0530

    Restore previous Yatrik UI

--- run.py
+++ run.py
@@ -22,3 +22,11 @@ if __name__ == "__main__":
         port=port,
         debug=False
     )
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
